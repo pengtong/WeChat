01b2e9dc8fc85c0691630ca2fe00addfe6adb3dd
Pengtong <user@UserdeMacBook-Pro.local>
2015-08-16 15:03:32 +0800

    Initial Commit

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by Pengtong on 15/8/16.
+//  Copyright (c) 2015年 Pengtong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by Pengtong on 15/8/16.
+//  Copyright (c) 2015年 Pengtong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
